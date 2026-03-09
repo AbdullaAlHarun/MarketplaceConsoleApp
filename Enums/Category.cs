8820bc0a607db0fe17700d48c3c521c6368d3bef
AbdullaAlHarun <abdhar01276@stud.noroff.no>
2026-03-10 00:38:48 +0100

    Add marketplace enums (Category, ItemCondition, ListingStatus)

---
+++ Enums/Category.cs
@@ -0,0 +1,11 @@
+namespace MarketplaceConsoleApp.Enums;
+
+public enum Category
+{
+    Electronics,
+    ClothingAccessories,
+    FurnitureHome,
+    BooksMedia,
+    SportsOutdoors,
+    Other
+}
